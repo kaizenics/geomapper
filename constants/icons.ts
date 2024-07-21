28295712b0f27f17ac91f472d0c92f025151a3c8
kaizenics <nicosejohnsoriano@gmail.com>
2024-07-22 01:51:42 +0800

    feat: Add icons module with map, home, and radar icons

---
+++ constants/icons.ts
@@ -0,0 +1,10 @@
+import map from '../assets/icons/map.png';
+import home from '../assets/icons/home.png';
+import radar from '../assets/icons/radar.png';
+
+export default {
+    map,
+    home,
+    radar
+}
+
