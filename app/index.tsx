391eda9a14008449a469484c2b265302a217f813
kaizenics <nicosejohnsoriano@gmail.com>
2024-07-21 20:18:24 +0800

    feat: initial commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="w-full">
+      <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
