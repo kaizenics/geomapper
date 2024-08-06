966681ac54f6c259fb28d8ec26f52ebebff2209f
kaizenics <nicosejohnsoriano@gmail.com>
2024-08-06 22:15:14 +0800

    feat: Update import paths for icons in constants/icons.ts

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,23 +1,24 @@
-import map from '../assets/icons/map.png';
-import home from '../assets/icons/home.png';
-import radar from '../assets/icons/radar.png';
-import profile from '../assets/icons/user.png';
-import option from '../assets/icons/option.png';
-import more from '../assets/icons/more.png';
-import menu from '../assets/icons/menu.png';
-import heavyRain from '../assets/icons/heavy-rain.png';
-import rainy from '../assets/icons/rainy-day.png';
-import sunny from '../assets/icons/sun.png';
-import windy from '../assets/icons/windy.png';
-import wind from '../assets/icons/wind.png';
-import pointer from '../assets/icons/pointer.png';
-import cloud from '../assets/icons/cloud.png';
-import partlyCloudy from '../assets/icons/cloudy-day.png';
-import foggy from '../assets/icons/foggy.png';
-import user from '../assets/icons/profile-user.png';
-import fish from '../assets/icons/fish.png';
-import down from '../assets/icons/down.png';
-import plus from '../assets/icons/plus.png';
+import map from '@/assets/icons/map.png';
+import home from '@/assets/icons/home.png';
+import radar from '@/assets/icons/radar.png';
+import profile from '@/assets/icons/user.png';
+import option from '@/assets/icons/option.png';
+import more from '@/assets/icons/more.png';
+import menu from '@/assets/icons/menu.png';
+import heavyRain from '@/assets/icons/heavy-rain.png';
+import rainy from '@/assets/icons/rainy-day.png';
+import sunny from '@/assets/icons/sun.png';
+import windy from '@/assets/icons/windy.png';
+import wind from '@/assets/icons/wind.png';
+import pointer from '@/assets/icons/pointer.png';
+import cloud from '@/assets/icons/cloud.png';
+import partlyCloudy from '@/assets/icons/cloudy-day.png';
+import foggy from '@/assets/icons/foggy.png';
+import user from '@/assets/icons/profile-user.png';
+import fish from '@/assets/icons/fish.png';
+import down from '@/assets/icons/down.png';
+import plus from '@/assets/icons/plus.png';
+import location from '@/assets/icons/location.png';
 
 export default {
     map,
@@ -40,4 +41,5 @@ export default {
     fish,
     down,
     plus,
+    location,
 }
